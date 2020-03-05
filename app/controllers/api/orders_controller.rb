9898200033c4c434d9c4cd41d071b69814083ef3
Thomas <thomas@craftacademy.se>
2020-03-05 14:51:05 +0100

    adds a PUT route to update an existing order

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,7 +1,16 @@
+# frozen_string_literal: true
+
 class Api::OrdersController < ApplicationController
-	def create
-		order = Order.create
-		order.order_items.create(product_id: params[:id])
-		render json: {message: 'The product has been added to your order'}
+  def create
+    order = Order.create
+    order.order_items.create(product_id: params[:product_id])
+    render json: { message: 'The product has been added to your order', order_id: order.id }
+  end
+
+  def update
+    order = Order.find(params[:id])
+    product = Product.find(params[:product_id])
+    order.order_items.create(product: product)
+    render json: { message: 'The product has been added to your order', order_id: order.id }
   end
 end
